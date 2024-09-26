5b35d51427003bd3f4bdbadaddbcf05f8968a7b6
Brianna <bround22@mit.edu>
2024-09-26 00:56:21 -0400

    DID TODO 1-3

--- server/concepts/authenticating.ts
+++ server/concepts/authenticating.ts
@@ -36,7 +36,12 @@ export default class AuthenticatingConcept {
     // TODO 1: implement this operation
     //  - use this.users.readOne(..)
     //  - don't include the password (we've provided a helper function you can use!)
-    throw new Error("Not implemented!");
+    const user = await this.users.readOne({"_id": _id});
+    if (!user){
+      throw new Error("User doesn't exist!")
+    }
+    const redactedUser = this.redactPassword(user);
+    return redactedUser;
   }
 
   async getUsers(username?: string) {
@@ -58,7 +63,10 @@ export default class AuthenticatingConcept {
     // TODO 2: implement this operation
     //  - use this.users.partialUpdateOne(..)
     //  - maintain the invariant that usernames are unique (we've provided a helper function!)
-    throw new Error("Not implemented!");
+    this.assertUsernameUnique(username);
+    const filter = {"_id": _id};
+    const updateQuery = {"username": username};
+    return await this.users.partialUpdateOne(filter, updateQuery);
   }
 
   async delete(_id: ObjectId) {
